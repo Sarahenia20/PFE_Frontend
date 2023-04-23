e0c9d3309efcc9868df9ae33fd6c98a70c59a490
Sarah Henia <95321098+Sarahenia20@users.noreply.github.com>
2023-04-24 00:16:07 +0100

    Add files via upload

---
+++ src/views/auth/SignIn.jsx
@@ -0,0 +1,71 @@
+import InputField from "components/fields/InputField";
+import Checkbox from "components/checkbox";
+import { Link } from "react-router-dom";
+
+export default function SignIn() {
+  return (
+    <div className="mt-8 mb-16 flex h-full w-full items-center justify-center px-2 md:mx-0 md:px-0 lg:mb-10 lg:items-center lg:justify-start">
+      {/* Sign in section */}
+      <div className="mt-[10vh] w-full max-w-full flex-col items-center md:pl-4 lg:pl-0 xl:max-w-[420px]">
+      <h4 className="mb-2.5 text-5xl font-bold text-navy-700 dark:text-white">
+         Welcome Back To TASKIFY ! 
+        </h4>
+        <h4 className="mb-2.5 text-4xl font-bold text-navy-700 dark:text-white">
+          Sign In
+        </h4>
+        <p className="mb-9 ml-1 text-base text-gray-600">
+          Enter your email and password to sign in!
+        </p>
+        
+        {/* Email */}
+        <InputField
+          variant="auth"
+          extra="mb-3"
+          label="Email*"
+          placeholder="mail@simmmple.com"
+          id="email"
+          type="text"
+        />
+
+        {/* Password */}
+        <InputField
+          variant="auth"
+          extra="mb-3"
+          label="Password*"
+          placeholder="Min. 8 characters"
+          id="password"
+          type="password"
+        />
+        {/* Checkbox */}
+        <div className="mb-4 flex items-center justify-between px-2">
+          <div className="flex items-center">
+            <Checkbox />
+            <p className="ml-2 text-sm font-medium text-navy-700 dark:text-white">
+              Keep me logged In
+            </p>
+          </div>
+          <Link
+            className="text-sm font-medium text-amber-500 hover:text-amber-600 dark:text-white"
+            to="/auth/reset-password"
+          >
+            Forgot Password?
+          </Link>
+        </div>
+        <button className="linear mt-2 w-full rounded-xl bg-amber-500 py-[12px] text-base font-medium text-white transition duration-200 hover:bg-amber-600 active:bg-amber-700 dark:bg-amber-400 dark:text-white dark:hover:bg-amber-300 dark:active:bg-amber-200">
+          Sign In
+        </button>
+        <div className="mt-4">
+          <span className=" text-sm font-medium text-navy-700 dark:text-gray-600">
+            Not registered yet?
+          </span>
+          <a
+            href="/auth/register"
+            className="ml-1 text-sm font-medium text-amber-500 hover:text-amber-600 dark:text-white"
+          >
+            Create an account
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
